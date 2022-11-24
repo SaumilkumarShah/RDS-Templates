e65b9e29708f58d67ac4d7f6391b8351c9faf712
Saumilkumar Kiritkumar Shah <76719429+SaumilkumarShah@users.noreply.github.com>
2022-11-24 10:42:43 +0000

    Update script.ps1

--- wvd-sh/arm-template-customization/script.ps1
+++ wvd-sh/arm-template-customization/script.ps1
@@ -1,18 +1,21 @@
-﻿##############################################################
-#  Run the Virtual Desktop Optimization Tool (VDOT)
-##############################################################
-# https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
+# ﻿##############################################################
+# #  Run the Virtual Desktop Optimization Tool (VDOT)
+# ##############################################################
+# # https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
 
-# Download VDOT
-$URL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
-$ZIP = 'VDOT.zip'
-Invoke-WebRequest -Uri $URL -OutFile $ZIP -ErrorAction 'Stop'
+# # Download VDOT
+# $URL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
+# $ZIP = 'VDOT.zip'
+# Invoke-WebRequest -Uri $URL -OutFile $ZIP -ErrorAction 'Stop'
 
-# Extract VDOT from ZIP archive
-Expand-Archive -LiteralPath $ZIP -Force -ErrorAction 'Stop'
+# # Extract VDOT from ZIP archive
+# Expand-Archive -LiteralPath $ZIP -Force -ErrorAction 'Stop'
     
-# Run VDOT
-& .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -AcceptEULA -Restart
+# # Run VDOT
+# & .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -AcceptEULA -Restart
 
-Start-Process -FilePath "C:\newscaler\Executefileshare.bat"
+# Start-Process -FilePath "C:\newscaler\Executefileshare.bat"
 
+Set-Location -Path c:\newscaler
+
+.\Executefileshare.bat
